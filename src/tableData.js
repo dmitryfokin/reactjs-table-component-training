c9ec2e4a148f7be16de66a66382b3e03abd5f845
Dmitry Fokin <dkoder.dvl@gmail.com>
2020-06-08 12:31:08 +0300

    create table data

---
+++ src/tableData.js
@@ -0,0 +1,40 @@
+export default {
+  tableData: {
+    rows: {
+      1: {
+        rowId: 1,
+        idTask: 1,
+        description: 'Задача 1',
+        status: 'new',
+      },
+      2: {
+        rowId: 2,
+        idTask: 2,
+        description: 'Задача 1',
+        status: 'new',
+      },
+      3: {
+        rowId: 3,
+        idTask: 3,
+        description: 'Задача 1',
+        status: 'new',
+      },
+    },
+  },
+  viewSettings: {
+    columns: [
+      {
+        name: 'description',
+        description: 'Описание задачи',
+        show: true,
+        disable: false,
+      },
+      {
+        name: 'status',
+        description: 'Статус',
+        show: true,
+        disable: false,
+      },
+    ],
+  },
+}
